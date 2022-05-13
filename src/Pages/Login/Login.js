49a78aba7d3e27f65662b619bb544cbde71e3de8
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-05-13 11:03:16 +0600

    Firebase auth added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login component.</h2>
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user)
+    }
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-center text-2xl font-normal">Login</h2>
+          <div class="divider">OR</div>
+          <button onClick={()=>signInWithGoogle()} class="btn btn-outline ">CONTINUE WITH GOOGLE</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
